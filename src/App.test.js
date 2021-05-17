347fec25ea4e5c1fc4bd49ec1784b77f8a17416f
superman566 <guochaohetony@gmail.com>
2021-05-17 18:42:03 -0400

    install enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find('[data-test="App"]')).toExist();
+  expect(wrapper.find('[data-test="App"]')).toHaveProp('title','app');
 });
